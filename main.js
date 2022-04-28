e14b78ad39d6fa7550d63195528edc0149f5159f
mykolassl <emykolas@gmail.com>
2022-04-28 17:48:10 +0300

    Add score tracking

--- main.js
+++ main.js
@@ -9,19 +9,41 @@ function playRound() {
     let playerChoice = prompt('Rock, paper or scissors?').toLowerCase();
     
     if(computerChoice === playerChoice) {
-        alert(`It's a tie! Your and computer's choice was ${playerChoice}`);
+        console.log(`It's a tie! Your and computer's choice was ${playerChoice}`);
+        return 'tie';
     } else if((playerChoice === 'scissors' && computerChoice === 'paper') ||
               (playerChoice === 'rock' && computerChoice === 'scissors') ||
               (playerChoice === 'paper' && computerChoice === 'rock')) {
-        alert(`You win! ${playerChoice} beats ${computerChoice}`);
+        console.log(`You win! ${playerChoice} beats ${computerChoice}`);
+        return 'win';
     } else {
-        alert(`You lose! ${computerChoice} beats ${playerChoice}`);
+        console.log(`You lose! ${computerChoice} beats ${playerChoice}`);
+        return 'loss';
     }
 }
 
 function startGame() {
+    let playerWins = 0;
+    let computerWins = 0;
+
     for(let i = 0; i < 5; i++) {
-        playRound();
+        let roundResult = playRound();
+
+        if(roundResult === 'win') {
+            playerWins++;
+        } else if(roundResult === 'loss') {
+            computerWins++;
+        }
+
+        console.log(`Player score: ${playerWins}\nComputer score: ${computerWins}`)
+    }
+
+    if(playerWins === computerWins) {
+        console.log("It's a tie!");
+    } else if(playerWins > computerWins) {
+        console.log("You won!");
+    } else {
+        console.log("You lost!");
     }
 }
 
